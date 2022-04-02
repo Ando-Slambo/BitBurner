554dd7c52d2cb216c77b23f5e8b880d3b7b36f02
Andoxico <69324424+Andoxico@users.noreply.github.com>
2022-04-02 06:30:51 -0400

    Delete cost.js

--- cost.js
+++
@@ -1,8 +0,0 @@
-/** @param {import("../.vscode").NS} ns */
-export async function main(ns) {
-    for (var i = 1; i < 21; i++){
-        var ramAmount = Math.pow(2, i);
-        const cost = ns.getPurchasedServerCost(ramAmount);
-        ns.tprint(ramAmount + "GB :" + ns.nFormat(cost, "$0.00a"));
-    }
-}
