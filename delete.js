848e594e0ae58b462499fed2f7585f2464cd11dc
Andoxico <69324424+Andoxico@users.noreply.github.com>
2022-04-02 06:30:56 -0400

    Delete delete.js

--- delete.js
+++
@@ -1,7 +0,0 @@
-/** @param {import("../.vscode").NS} ns */
-export async function main(ns) {
-    const server = ns.args[0];
-    if (ns.deleteServer(server)) {
-        ns.tprint("Deleted " + server);
-    }
-}
