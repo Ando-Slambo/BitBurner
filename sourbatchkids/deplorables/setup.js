afade0faf899599a0ce1e55a74eacf4b2ed62dbc
Andoxico <69324424+Andoxico@users.noreply.github.com>
2022-04-03 02:18:18 -0400

    Update setup.js
    Removed variables that were only being used once

--- sourbatchkids/deplorables/setup.js
+++ sourbatchkids/deplorables/setup.js
@@ -5,37 +5,29 @@ const monitor = "/sourbatchkids/deplorables/monitor.js";
 /** @param {import("../../.vscode").NS} ns */
 export async function main(ns) {
     const batchTarget = ns.args[0];
-    const modifier = ns.args[1] || undefined;
-    const percentToSteal = ns.args[2] || 20;
+    //const modifier = ns.args[1] || undefined;     //used to be used in last if statement
+    //const percentToSteal = ns.args[2] || 20;      //(must still be passed if restarting batches)
     const host = ns.getHostname();
-    const growRam = ns.getScriptRam(grow);
-    const weakenRam = ns.getScriptRam(weaken);
     const weakenTime = ns.getWeakenTime(batchTarget);
-
     const maxMoney = ns.getServerMaxMoney(batchTarget);
-    const currentMoney = ns.getServerMoneyAvailable(batchTarget);
     const minSecurity = ns.getServerMinSecurityLevel(batchTarget);
-    const currentSecurity = ns.getServerSecurityLevel(batchTarget);
 
     //Initial weaken threads
-    const securityDifferential = currentSecurity - minSecurity;
-    var weakenThreads = securityDifferential / 0.05;
+    var weakenThreads = (ns.getServerSecurityLevel(batchTarget) - minSecurity) / 0.05;
 
     //Grow threads
-    const growthMultiplier = maxMoney / currentMoney;
-    var growThreads = ns.growthAnalyze(batchTarget, growthMultiplier);
+    var growThreads = ns.growthAnalyze(batchTarget, ( maxMoney / ns.getServerMoneyAvailable(batchTarget) ));
     if (growThreads < 1) {
         growThreads = 1;
     }
-    const growthSecurityRaise = growThreads * 0.004;
-    weakenThreads += growthSecurityRaise / 0.05;
+    weakenThreads += (growThreads * 0.004) / 0.05;
 
     weakenThreads = Math.ceil(weakenThreads / batchesRequired);
     growThreads = Math.ceil(growThreads / batchesRequired);
 
     //RAM requirements
     const availRam = ns.getServerMaxRam(host) - ns.getServerUsedRam(host);
-    var requiredRam = (growRam * growThreads) + (weakenRam * weakenThreads);
+    var requiredRam = (ns.getScriptRam(grow) * growThreads) + (ns.getScriptRam(weaken) * weakenThreads);
 
     //Splits the scripts into multiple batches if they cannot all be run at once;
     var batchesRequired = 1;
@@ -59,7 +51,7 @@ export async function main(ns) {
     ns.tprintf("\nMin Security: " + minSecurity);
     ns.tprintf("Current security: " + ns.getServerSecurityLevel(batchTarget));
 
-    if (modifier == "restart") {
-        ns.run(monitor, 1, batchTarget, percentToSteal);
+    if (ns.args[1] == "restart") {
+        ns.run(monitor, 1, batchTarget, ns.args[2]);
     }
 }
