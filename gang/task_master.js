e1edfbb39e35d7ffc089d94696dee2efaeb396c6
Andoxico <69324424+Andoxico@users.noreply.github.com>
2022-04-05 06:11:50 -0400

    Update task_master.js
    implemented missing behavior

--- gang/task_master.js
+++ gang/task_master.js
@@ -19,10 +19,8 @@ export async function main(ns) {
         await ns.sleep(10000);
 
         //recruit if able and re-evaluate the members array
-        if (ns.gang.canRecruitMember()) {
-            Recruiter(ns);
-            members = ns.gang.getMemberNames();
-        }
+        if (ns.gang.canRecruitMember()) { Recruiter(ns) }
+        if (members.length < ns.gang.getMemberNames().length) { members = ns.gang.getMemberNames() }
 
         //set members to train their needed stat
         await Trainer(ns, members);
@@ -34,21 +32,24 @@ export async function main(ns) {
         else if (bonusTimeEnd) { continue }
 
         for (const member of members) {
-            const lowest_stat = GetLowestStat(ns, member);
+            const lowest_stat = await GetLowestStat(ns, member);
+            const lowest_asc_mult = Math.min(...await GetAscensionMults(ns, member));
 
             //if member's ascension multiplier is at least 30 they are done training, set to territory warfare
-            if (Math.min(...GetAscensionMults(ns, member) >= 30)) { 
-                if (ns.gang.getMemberInformation().task != "Territory Warfare") { ns.gang.setMemberTask(member, "Territory Warfare") }
+            if (lowest_asc_mult >= 30) { 
+                if (ns.gang.getMemberInformation(member).task != "Territory Warfare") { ns.gang.setMemberTask(member, "Territory Warfare") }
                 continue;
             }
             //if member's lowest stat is at least 1000 they are temporarily done training, set to human trafficking to gain rep to recruit
             if (lowest_stat >= 1000) { 
-                if (ns.gang.getMemberInformation().task != "Human Trafficking") { ns.gang.setMemberTask(member, "Human Trafficking") }
+                if (members.length >= 12) { continue }
+                if (ns.gang.getMemberInformation(member).task != "Human Trafficking") { ns.gang.setMemberTask(member, "Human Trafficking") }
                 continue;
             }
             //if member's lowest stat is at least 100 they are temporarily done training, set to mug people to gain rep to recruit
             if (lowest_stat >= 100) { 
-                if (ns.gang.getMemberInformation().task != "Mug People") { ns.gang.setMemberTask(member, "Mug People") }                
+                if (members.length >= 7) { continue }
+                if (ns.gang.getMemberInformation(member).task != "Mug People") { ns.gang.setMemberTask(member, "Mug People") }                
             }
         }
     }
