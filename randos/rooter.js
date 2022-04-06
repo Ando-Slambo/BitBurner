b255b7598d1e36b05908ba161861b87ca80798f6
Andoxico <69324424+Andoxico@users.noreply.github.com>
2022-04-06 03:17:55 -0400

    Update rooter.js

--- randos/rooter.js
+++ randos/rooter.js
@@ -2,7 +2,7 @@ import { Root } from "/deploy/utils.js"
 
 /** @param {import("../.vscode").NS} ns */
 export async function main(ns) {
-	const target = ns.args[0];
+    const target = ns.args[0];
     
     if (await Root(ns, target)) { ns.tprintf("Rooted " + target) }
     else { ns.tprintf("Unable to root " + target) }
