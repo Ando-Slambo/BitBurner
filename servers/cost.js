c877f1241ab52f2cba92b30146fd3238d4395681
Andoxico <69324424+Andoxico@users.noreply.github.com>
2022-04-19 04:09:10 -0400

    Update cost.js
    updated to use GiB instead of GB

--- servers/cost.js
+++ servers/cost.js
@@ -3,6 +3,6 @@ export async function main(ns) {
     for (var i = 1; i < 21; i++){
         const ramAmount = Math.pow(2, i);
         const cost = ns.getPurchasedServerCost(ramAmount);
-        ns.tprint(ns.nFormat(ramAmount * 1000000000, "0b") + ": " + ns.nFormat(cost, "$0.00a") + " - " + ramAmount);
+        ns.tprint(ns.nFormat(ramAmount * 1073741824, "0ib") + ": " + ns.nFormat(cost, "$0.00a") + " - " + ramAmount);
     }
 }
